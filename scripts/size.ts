d4a148e52360eec0e5d25f96bfdce39f480d022f
Dmitrii Eroshenko <dmitrii.eroshenko@zalando.de>
2023-04-03 23:37:39 +0200

    Size info

--- scripts/size.ts
+++ scripts/size.ts
@@ -1,12 +1,24 @@
 import { promises } from 'fs';
 import { join } from 'path';
 
+const formatBytes = (
+  bytes: number,
+  decimals: number) =>  {
+    if(bytes == 0) return '0 Bytes';
+    var k = 1024,
+      dm = decimals || 2,
+      sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+      i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+  }
+
 type SizeInfo = {
   [scriptName: string]: {
     [extention: string]: {
       current: number,
       before: number,
-      diff: number
+      currentString: string,
+      beforeString: string
     }
   }
 }
@@ -29,8 +41,9 @@ const run = async () => {
       const before = privious[shortName]?.[ext]?.current || stats.size;
       result[shortName][ext] = {
         current: stats.size,
-        before, 
-        diff: stats.size - before
+        before,
+        currentString: formatBytes(stats.size, 3),
+        beforeString: formatBytes(before, 3)
       }
     }
   }
