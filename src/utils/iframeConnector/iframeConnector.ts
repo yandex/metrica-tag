bea62ea58d66b4a2f499cb08126a44cac384849e
Dmitrii Eroshenko <dmitrii.eroshenko@zalando.de>
2024-01-23 15:10:55 +0100

    run again

--- src/utils/iframeConnector/iframeConnector.ts
+++ src/utils/iframeConnector/iframeConnector.ts
@@ -195,9 +195,6 @@ export const handleInputMessage = (
     counterInfo: CounterInfo,
     event: MessageEvent,
 ) => {
-    if (event.data[0] !== '{') {
-        return;
-    }
     let messageInfo: FullMessage | null = null;
     let meta: string | null = null;
     const state = getIframeState(ctx);
