3cad5df65cc1df27ab82474e57d71169f845f106
cvgalagan <cvgalagan@yandex-team.com>
2024-01-24 11:58:01 +0300

    Added Beacon API to the artificial hit provider

--- src/providers/artificialHit/index.ts
+++ src/providers/artificialHit/index.ts
@@ -4,7 +4,7 @@ import { commonMiddlewares, providerMiddlewareList } from 'src/middleware';
 import { providersSync } from 'src/providersEntrypoint';
 import { providerMap } from 'src/sender';
 import { SenderWatch, useSenderWatch } from 'src/sender/watch';
-import { nameMap, withoutBeacon } from 'src/transport';
+import { fullList, nameMap } from 'src/transport';
 import { ctxErrorLogger } from 'src/utils/errorLogger';
 import { artificialHitProvider } from './artificialHit';
 import { ARTIFICIAL_HIT_PROVIDER } from './const';
@@ -25,6 +25,6 @@ export const initProvider = () => {
         providersSync.push(ctxErrorLogger('p.ar', artificialHitProvider));
         providerMap[ARTIFICIAL_HIT_PROVIDER] = useSenderWatch;
         providerMiddlewareList[ARTIFICIAL_HIT_PROVIDER] = commonMiddlewares;
-        nameMap[ARTIFICIAL_HIT_PROVIDER] = withoutBeacon;
+        nameMap[ARTIFICIAL_HIT_PROVIDER] = fullList;
     }
 };
