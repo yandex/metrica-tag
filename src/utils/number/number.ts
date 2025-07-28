cdb75e6d026ba4c354de666ead06621f1a02f239
sabio <sabio@yandex-team.com>
2025-07-28 14:18:03 +0300

    Max int value commit_hash:8e6a79a92f025945dbca9976acf185bde2f34e20

--- src/utils/number/number.ts
+++ src/utils/number/number.ts
@@ -1,6 +1,8 @@
 import { protoToString } from 'src/utils/string';
 import { curry2SwapArgs } from '../function/curry';
 
+export const MAX_INT_32 = 2147483647;
+
 export const isNumber = (ctx: Window, obj: any): obj is number => {
     return (
         ctx.isFinite(obj) &&
