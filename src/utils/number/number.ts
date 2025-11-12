b376613e6d5e5086661f3152df378a73d5525dec
cvgalagan <cvgalagan@yandex-team.com>
2025-11-12 12:40:31 +0300

    Added safe number type checking commit_hash:94c46fca9155a55944d0fef56d6eecdf4a872c0f

--- src/utils/number/number.ts
+++ src/utils/number/number.ts
@@ -11,6 +11,19 @@ export const isNumber = (ctx: Window, obj: any): obj is number => {
     );
 };
 
+/**
+ * In case of monkey patching toString and indexOf methods
+ * or passing an object without prototype,
+ * isNumber throws a type error
+ */
+export const isNumberSafe = (ctx: Window, obj: any): obj is number => {
+    try {
+        return isNumber(ctx, obj);
+    } catch {
+        return false;
+    }
+};
+
 export const toNumber = (obj: any): number => +obj;
 
 export const parseIntSafe = (val: string) => {
